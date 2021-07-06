5f460de89d27a6833afd46c62ff9db517b4f4a96
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-06 13:01:39 +0100

    Commit

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pytube
+import os
 
 url = input('Url musica que deseja fazer o download: ')
 yt = pytube.YouTube(url)
 video = yt.streams.filter(only_audio=True).first()
 destino = input('Digite o endereço da pasta que deseja guardar a música: ')
-music = video.download(output_path=destino)
-print(music)
+ficheiro_music = video.download(output_path=destino)
+# Remove a extensão mp4
+base, ext = os.path.splitext(ficheiro_music)
+# Adiciona a extensão mp3
+musica = base + '.mp3'
+# Renomeia o ficheiro
+os.rename(ficheiro_music, musica)
