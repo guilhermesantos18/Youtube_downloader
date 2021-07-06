5603bd28c77fc4be84563da6f3184c63ec65fe76
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-06 12:07:02 +0100

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pytube
 
-url = ''
+url = input('Url musica que deseja fazer o download: ')
 yt = pytube.YouTube(url)
 video = yt.streams.filter(only_audio=True).first()
-video.download()
+destino = input('Digite o endereço da pasta que deseja guardar a música: ')
+music = video.download(output_path=destino)
+print(music)
